48cdc10188a985ff33dc5dd250f07f7dac19c8fb
GAO <gao.yu@husky.neu.edu>
2019-09-24 17:03:17 +0800

    message doing

--- core/model/message.go
+++ core/model/message.go
@@ -1,24 +1,27 @@
 package model
 
 const (
-	MessageTypeCoolForContent    = 0
-	MessageTypeCoolForComment    = 1
-	MessageTypeCommentForComment = 2
-	MessageTypeCommentForContent = 3
-	MessageTypeBanForContent     = 4
-	MessageTypeBanForComment     = 5
-	MessageTypeSystem            = 6
+	MessageTypePrivate = 0
+	MessageTypePublic  = 1 //  not use
+	MessageTypeGlobal  = 2
 )
 
+// https://my.oschina.net/cmcm/blog/397323
+// Message inside
 type Message struct {
-	Id              int64  `json:"id" xorm:"bigint pk autoincr"`
-	TargetUserId    int64  `json:"target_user_id" xorm:"bigint index"`
-	CreateTime      int64  `json:"create_time"`
-	Status          int    `json:"status" xorm:"not null comment('0 waiting,1 read') TINYINT(1) index"`
-	MessageType     int    `json:"message_type" xorm:"not null TINYINT(1) index"`
-	ContentId       int64  `json:"content_id" xorm:"bigint index"` // 034
-	OriginUserId    int64  `json:"origin_user_id" xorm:"bigint index"`
-	OriginCommentId int64  `json:"origin_comment_id" xorm:"bigint index"` // 23
-	TargetCommentId int64  `json:"target_comment_id" xorm:"bigint index"` // 125
-	MessageBody     string `json:"message_body"`                          // 6
+	Id            int64 `json:"id" xorm:"bigint pk autoincr"`
+	ReceiveUserId int64 `json:"receive_user_id" xorm:"bigint index"`
+	CreateTime    int64 `json:"create_time"`
+	ReadTime      int64 `json:"read_time"`
+	DeleteTime    int64 `json:"delete_time"`
+	Status        int   `json:"status" xorm:"not null comment('0 waiting,1 read,2 delete') TINYINT(1) index"`
+}
+
+type MessageText struct {
+	Id         int64  `json:"id" xorm:"bigint pk autoincr"`
+	SendUserId int64  `json:"send_user_id" xorm:"bigint index"`
+	CreateTime int64  `json:"create_time"`
+	Body       string `json:"body"`
+	GroupId    int64  `json:"group_id"`
+	Type       int    `json:"type"`
 }
